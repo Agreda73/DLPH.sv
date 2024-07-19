3fb45b1dd0544a0ad496a79cd130559734bced08
katin1522 <20220026@ricaldone.edu.sv>
2024-07-18 22:01:09 -0600

    4 pantallas

---
+++ app/src/main/java/roberto/agreda/dlphsv/AgregarExpediente.kt
@@ -0,0 +1,20 @@
+package roberto.agreda.dlphsv
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class AgregarExpediente : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_agregar_expediente)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
