07c42c89cf92e9417d0dba0a6467b60084d369e1
fabiola <20230033@ricaldone.edu.sv>
2024-07-19 23:23:10 -0600

    recu

---
+++ app/src/main/java/roberto/agreda/dlphsv/enviarCorreo.kt
@@ -0,0 +1,46 @@
+package roberto.agreda.dlphsv
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.util.Properties
+import javax.mail.Message
+import javax.mail.MessagingException
+import javax.mail.PasswordAuthentication
+import javax.mail.Session
+import javax.mail.Transport
+import javax.mail.internet.InternetAddress
+import javax.mail.internet.MimeMessage
+
+suspend fun enviarCorreo(receptor: String, sujeto: String, mensaje: String) = withContext(
+    Dispatchers.IO) {
+    // Configuración del servidor SMTP
+    val props = Properties().apply {
+        put("mail.smtp.host", "smtp.gmail.com")
+        put("mail.smtp.socketFactory.port", "465")
+        put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory")
+        put("mail.smtp.auth","true")
+        put("mail.smtp.port","465")
+    }
+        val session = Session.getInstance(props, object : javax.mail.Authenticator() {
+            override fun getPasswordAuthentication(): PasswordAuthentication {
+                return PasswordAuthentication("dlphcontac@gmail.com","trma nrfw jkcn gwtv")
+            }
+        })
+
+        // Hacemos el envío
+        try {
+            val message = MimeMessage(session).apply {
+                //Con que correo enviaré el mensaje
+                setFrom(InternetAddress("dlphcontac@gmail.com"))
+                addRecipient(Message.RecipientType.TO, InternetAddress(receptor))
+                subject = sujeto
+                setText(mensaje)
+            }
+            Transport.send(message)
+            println("Correo enviado satisfactoriamente")
+        } catch (e: MessagingException) {
+            e.printStackTrace()
+            println("CORREO NO ENVIADO ${e.message}")
+        }
+
+    }
