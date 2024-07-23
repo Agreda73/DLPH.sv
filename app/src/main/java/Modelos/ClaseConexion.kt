c916b75bd8c4ff7acef87b749432c0bdb0eaed48
20230064 <20230006@ricaldone.edu.sv>
2024-07-23 15:41:30 -0600

    jaskdj

---
+++ app/src/main/java/Modelos/ClaseConexion.kt
@@ -0,0 +1,16 @@
+package Modelos
+
+import java.sql.Connection
+
+class ClaseConexion {
+     fun cadenaConexion(): Connection {
+            try {
+                val url = "jdbc:oracle:thin:@10.10.0.29:1521:xe"
+                val user = "SYSTEM"
+                val password = "ITR2024"
+            }
+
+     }
+
+
+}
