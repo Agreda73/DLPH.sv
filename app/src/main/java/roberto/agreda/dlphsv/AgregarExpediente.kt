b8bf2c398851e1a655857124b8327950b2c4d519
katin1522 <20220026@ricaldone.edu.sv>
2024-07-28 23:03:23 -0600

    Validacion de campos

--- app/src/main/java/roberto/agreda/dlphsv/AgregarExpediente.kt
+++ app/src/main/java/roberto/agreda/dlphsv/AgregarExpediente.kt
@@ -1,6 +1,8 @@
 package roberto.agreda.dlphsv
 
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,36 @@ class AgregarExpediente : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
         }
+        val txtAgregarExpediente = findViewById<EditText>(R.id.txtAgregarExpediente)
+
+        if (txtAgregarExpediente.text.isEmpty() || txtAgregarExpediente.text.trim().isEmpty()) {
+            txtAgregarExpediente.error = "Campo requerido"
+        } else if (txtAgregarExpediente.text.length < 25) {
+            txtAgregarExpediente.error = "Mínimo 25 caracteres"
+        }
+
+        val sphabitacionAE = findViewById<Spinner>(R.id.sphabitacionAE)
+
+        if (sphabitacionAE.selectedItemPosition == 0) {
+        }
+
+        val txtDiagnostiAe = findViewById<EditText>(R.id.txtDiagnostiAe)
+
+        if (txtDiagnostiAe.text.isEmpty() || txtDiagnostiAe.text.trim().isEmpty()) {
+            txtDiagnostiAe.error = "Campo requerido"
+        } else if (txtDiagnostiAe.text.length < 50) {
+            txtDiagnostiAe.error = "Mínimo 50 caracteres"
+        }
+
+        val txtHistoriaExpediente = findViewById<EditText>(R.id.txtHistorialExpediente)
+
+        if ( txtHistoriaExpediente.text.isEmpty() || txtHistoriaExpediente.text.trim().isEmpty()) {
+             txtHistoriaExpediente.error = "Campo requerido"
+        } else if ( txtHistoriaExpediente.text.length < 25) {
+            txtHistoriaExpediente.error = "Mínimo 25 caracteres"
+        }
+
     }
 }
