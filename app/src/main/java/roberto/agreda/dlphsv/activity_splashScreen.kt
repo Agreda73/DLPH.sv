3264d80d3d56255310cd7cc2636acb488f1eeea9
Fabiola <20230033@ricaldone.edu.sv>
2024-07-28 18:05:52 -0600

    splash screen

---
+++ app/src/main/java/roberto/agreda/dlphsv/activity_splashScreen.kt
@@ -0,0 +1,31 @@
+package roberto.agreda.dlphsv
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class activity_splashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash_screen)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.splash_screen)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        GlobalScope.launch(Dispatchers.Main) {
+            delay(4000)
+            startActivity(Intent(this@activity_splashScreen, MainActivity::class.java))
+            finish()
+        }
+    }
+}
